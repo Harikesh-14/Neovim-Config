f268f8a1747dceb9ce0f926b68a8c0a56f50c3aa
harikeshranjan <harikeshranjansinha14@gmail.com>
2025-04-22 14:09:47 +0000

    Final Push

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -24,6 +24,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.lang.java" },
     { import = "plugins" },
   },
   defaults = {
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -10,6 +10,14 @@ return {
         "tailwindcss-language-server",
         "typescript-language-server",
         "css-lsp",
+        "clangd",
+        "goimports",
+        "gofumpt",
+        "gomodifytags",
+        "impl",
+        "delve",
+        "java-debug-adapter",
+        "java-test",
       })
     end,
   },
@@ -21,6 +29,8 @@ return {
       inlay_hints = { enabled = true },
       -- @type lspconfig.options
       servers = {
+        jdtls = {},
+        pyright = {},
         cssls = {},
         tailwindcss = {},
         ts_ls = {
@@ -119,8 +129,61 @@ return {
             },
           },
         },
+        gopls = {
+          settings = {
+            gopls = {
+              gofumpt = true,
+              codelenses = {
+                gc_details = false,
+                generate = true,
+                regenerate_cgo = true,
+                run_govulncheck = true,
+                test = true,
+                tidy = true,
+                upgrade_dependency = true,
+                vendor = true,
+              },
+              hints = {
+                assignVariableTypes = true,
+                compositeLiteralFields = true,
+                compositeLiteralTypes = true,
+                constantValues = true,
+                functionTypeParameters = true,
+                parameterNames = true,
+                rangeVariableTypes = true,
+              },
+              usePlaceholders = true,
+              completeUnimported = true,
+              staticcheck = true,
+              directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+              semanticTokens = true,
+            },
+          },
+        },
       },
-      setup = {},
+      setup = {
+        gopls = function(_, opts)
+          -- workaround for gopls not supporting semanticTokensProvider
+          -- https://github.com/golang/go/issues/54531#issuecomment-1464982242
+          LazyVim.lsp.on_attach(function(client, _)
+            if not client.server_capabilities.semanticTokensProvider then
+              local semantic = client.config.capabilities.textDocument.semanticTokens
+              client.server_capabilities.semanticTokensProvider = {
+                full = true,
+                legend = {
+                  tokenTypes = semantic.tokenTypes,
+                  tokenModifiers = semantic.tokenModifiers,
+                },
+                range = true,
+              }
+            end
+          end, "gopls")
+          -- end workaround
+        end,
+        jdtls = function()
+          return true
+        end,
+      },
     },
   },
   {
@@ -146,4 +209,5 @@ return {
       table.insert(opts.sources, { name = "emoji" })
     end,
   },
+  { "folke/which-key.nvim" },
 }
